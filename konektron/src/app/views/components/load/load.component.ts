03ef34a43aa0aba910708dff3271c5ffa0f5a8a7
Ivam <ivam.developer@gmail.com>
2021-02-25 22:56:31 -0300

    New_components_02

--- konektron/src/app/views/components/load/load.component.ts
+++ konektron/src/app/views/components/load/load.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-load',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.navHome();
   }
+
+  // Function redirect home
+  // Begin
+  navHome() {
+    setTimeout(() => {
+      this.router.navigate(['home']);
+    }, 3000);
+  }
+  // End
 
 }
