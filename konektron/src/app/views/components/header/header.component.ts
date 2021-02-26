dc78e88e189a61b298a2e0299d08861c60397e94
Ivam <ivam.developer@gmail.com>
2021-02-26 20:37:19 -0300

    Build_nav_01

--- konektron/src/app/views/components/header/header.component.ts
+++ konektron/src/app/views/components/header/header.component.ts
@@ -12,4 +12,18 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+  openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById('logo').style.visibility = 'hidden';
+  }
+
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById('logo').style.visibility = 'visible';
+  }
+
 }
